88bc4b67e5b02b48f4f8e6e7fa31973974c2ce16
culi <culitif@tuta.io>
2021-06-21 17:08:45 -0700

    fix tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,11 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 import Friend from '../lib/models/Friend.js';
+import { jest } from '@jest/globals';
+
 
 describe('friend controller routes', () => {
+  jest.setTimeout(9999);
 
   let joey, noodle;
 
@@ -23,20 +26,22 @@ describe('friend controller routes', () => {
 
   it('POST new friend and send text', async () => {
     const input = joey;
-    const actual = await request(app)
+    const actual = (await request(app)
       .post('/api/v1/friends')
       .send(input)
-    ;
-    const expected = joey;
+    ).body;
+    
+    const expected = { id: expect.any(Number), ...joey };
 
     expect(actual).toEqual(expected);
   });
 
   it('GET friend by id', async () => {
     const input = await Friend.insert(joey);
-    const actual = await request(app)
+    const actual = (await request(app)
       .get(`/api/v1/friends/${input.id}`)
-    ;
+    ).body;
+
     const expected = { id: expect.any(Number), ...joey };
 
     expect(actual).toEqual(expected);
@@ -46,9 +51,10 @@ describe('friend controller routes', () => {
     joey = await Friend.insert(joey);
     noodle = await Friend.insert(noodle);
 
-    const actual = await request(app)
+    const actual = (await request(app)
       .get('/api/v1/friends')
-    ;
+    ).body;
+
     const expected = [joey, noodle];
 
     expect(actual).toEqual(expected);
@@ -58,7 +64,10 @@ describe('friend controller routes', () => {
     joey = await Friend.insert(joey);
     joey.name = 'Samantha';
 
-    const actual = (await request(app).put(`/api/v1/friends/${joey.id}`)).name;
+    const actual = (await request(app)
+      .put(`/api/v1/friends/${joey.id}`)
+      .send(joey)
+    ).body.name;
     const expected = 'Samantha';
 
     expect(actual).toEqual(expected);
@@ -66,9 +75,9 @@ describe('friend controller routes', () => {
 
   it('DELETE friend by id and send text', async () => {
     joey = await Friend.insert(joey);
-    const actual = await request(app)
+    const actual = (await request(app)
       .delete(`/api/v1/friends/${joey.id}`)
-    ;
+    ).body;
     const expected = joey;
 
     expect(actual).toEqual(expected);
